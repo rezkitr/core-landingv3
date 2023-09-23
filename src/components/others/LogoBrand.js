935e05b1ee43f12bd69daef8cae5215da96b4b82
Rezki Triandy <rezki.triandy@majoo.id>
2023-09-23 14:59:46 +0700

    chore: update logo brand

--- src/components/others/LogoBrand.js
+++ src/components/others/LogoBrand.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const LogoBrand = () => {
   return (
-    <div className="flex items-center gap-2">
+    <Link href="/" className="flex items-center gap-2">
       <Image src="/icons/logo.png" alt="logo" width={52} height={32} />
       <h5 className="logo-text font-semibold">GameManiac</h5>
-    </div>
+    </Link>
   )
 }
 
