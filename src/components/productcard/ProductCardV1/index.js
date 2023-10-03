f1e7d5d70f86c1699fd3f527d85cf5539b72f4f2
Rezki Triandy <rezki.triandy@majoo.id>
2023-10-03 15:29:48 +0700

    chore: add shadow layer product card

--- src/components/productcard/ProductCardV1/index.js
+++ src/components/productcard/ProductCardV1/index.js
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 
-import Button from '@/components/Button'
-
 const ProductCardV1 = ({ isPromo = false, className = '' }) => {
   return (
     <div
       className={`product-card relative h-64 w-[140px] max-w-[140px] overflow-hidden rounded-xl bg-primaryBlack md:h-72 md:w-[200px] md:max-w-[200px] ${className}`}
     >
       <div className="relative h-36 md:h-52">
+        <div className="absolute inset-0 z-10 bg-gradient-to-t from-white/50"></div>
         <Image
           src="/images/productcard-img.png"
           alt="product-img"
@@ -15,7 +14,7 @@ const ProductCardV1 = ({ isPromo = false, className = '' }) => {
           className="object-fill"
         />
       </div>
-      <div className="absolute inset-x-0 bottom-0">
+      <div className="absolute inset-x-0 bottom-0 z-20">
         <div className="absolute -top-4 right-0 h-6 w-2/5 rounded-t-lg bg-primaryBlack md:-top-6 md:rounded-t-2xl">
           <div className="absolute inset-x-0 bottom-2 mx-auto h-8 w-8 rounded-md bg-white p-2 outline outline-1 outline-offset-2 outline-white md:bottom-0 md:h-[42px] md:w-[42px]">
             <div className="relative h-full w-full">
