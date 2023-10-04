26fbc721ee00d2a4e0734f7cf91500b6358b5167
Rezki Triandy <rezki.triandy@majoo.id>
2023-10-04 09:36:50 +0700

    fix: fix responsive product card

--- src/components/productcard/ProductCardV1/index.js
+++ src/components/productcard/ProductCardV1/index.js
@@ -3,19 +3,25 @@ import Image from 'next/image'
 const ProductCardV1 = ({ isPromo = false, className = '' }) => {
   return (
     <div
-      className={`product-card relative h-64 w-[140px] max-w-[140px] overflow-hidden rounded-xl bg-primaryBlack md:h-72 md:w-[200px] md:max-w-[200px] ${className}`}
+      className={`product-card relative h-64 w-[140px] max-w-[140px] overflow-hidden rounded-xl md:h-72 md:w-[200px] md:max-w-[200px] ${className}`}
     >
-      <div className="relative h-36 md:h-52">
-        <div className="absolute inset-0 z-10 bg-gradient-to-t from-white/50"></div>
-        <Image
-          src="/images/productcard-img.png"
-          alt="product-img"
-          fill
-          className="object-fill"
-        />
+      <div className="h-full">
+        <div
+          className={`relative ${
+            isPromo ? 'h-[150px] md:h-52' : 'h-44 md:h-56'
+          }`}
+        >
+          <div className="absolute inset-0 z-10 bg-gradient-to-t from-white/50" />
+          <Image
+            src="/images/productcard-img.png"
+            alt="product-img"
+            fill
+            className="object-fill"
+          />
+        </div>
       </div>
-      <div className="absolute inset-x-0 bottom-0 z-20">
-        <div className="absolute -top-4 right-0 h-6 w-2/5 rounded-t-lg bg-primaryBlack md:-top-6 md:rounded-t-2xl">
+      <div className="absolute inset-x-0 bottom-0 z-10">
+        <div className="absolute -top-4 right-0 h-6 w-2/5 rounded-t-lg bg-primaryBlack md:-top-6 md:rounded-t-xl">
           <div className="absolute inset-x-0 bottom-2 mx-auto h-8 w-8 rounded-md bg-white p-2 outline outline-1 outline-offset-2 outline-white md:bottom-0 md:h-[42px] md:w-[42px]">
             <div className="relative h-full w-full">
               <Image
